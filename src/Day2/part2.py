f1ace2bcfe1b32ed96338c95e60aa6fe6cec3a52
Roni Eerikainen <reerikai@c1r6p17.hive.fi>
2025-12-03 13:48:41 +0200

    Day 2 done

--- src/Day2/part2.py
+++ src/Day2/part2.py
@@ -3,7 +3,7 @@ def main():
     with open("input.txt", "r") as file1:
         content = file1.read()
         res = 0
-        
+
         """Splitting the inputs into pairs into a list"""
         for pair in content.split(","):
             first_str, last_str = pair.split("-")
@@ -11,12 +11,25 @@ def main():
             last = int(last_str)
             for ID in range(first, last+1):
                 stringID = str(ID)
-                if (len(stringID) % 2 != 0):
-                    continue
-                firstpart, secondpart = stringID[:len(stringID)//2], stringID[len(stringID)//2:]
-                if (firstpart == secondpart):
-                    res = res + int(stringID)
+                if invalidID(stringID):
+                    res += int(stringID)
     print(res)
+
+def invalidID(stringID):
+        stringLen = len(stringID)
+        for patternSize in range(1, stringLen // 2 + 1):
+             if stringLen % patternSize != 0:
+                  continue
+             block = stringID[:patternSize]
+             res = block
+             number = stringLen // patternSize
+             for i in range(1, number):
+                  res += block
+             if res == stringID:
+                  return True
+        return False
+
+
 
 if __name__=="__main__":
     main()
