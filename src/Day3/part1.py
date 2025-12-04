20b37a352ab7369e3bc64add967bb4b76ffba5de
Roni <roni.eerikainen6@gmail.com>
2025-12-04 21:01:50 +0200

    Day 3 part 1 done

--- src/Day3/part1.py
+++ src/Day3/part1.py
@@ -1,22 +1,22 @@
 
 def main():
-    with open("test.txt", "r") as file1:
+    with open("input.txt", "r") as file1:
         
         result = 0
         content = file1.readlines()
         for line in content:
             largestFirst = 0
             largestBoth = 0
-            for curr, next in zip(line, line[1:]):
-                if curr.isdigit() and next.isdigit():
+            for i, curr in enumerate(line):
+                if curr.isdigit():
                     currNum = int(curr)
-                    nextNum = int(next)
-                    fullNumStr = curr + next
                     if currNum > largestFirst:
                         largestFirst = currNum
-                        if int(fullNumStr) > largestBoth:
-                            largestBoth = int(fullNumStr)
-                            print(largestBoth)
+                        for j, next in enumerate(line[i+1:], start=i+1):
+                            fullNumStr = curr + next
+                            if int(fullNumStr) > largestBoth:
+                                largestBoth = int(fullNumStr)
+
             result += largestBoth
         print(result)
                     
---
+++ src/Day3/part2.py
@@ -0,0 +1,13 @@
+
+def main():
+    with open("input.txt", "r") as file1:
+        
+        result = 0
+        content = file1.readlines()
+        for line in content:
+            print("yes")
+        print(result)
+                    
+                
+if __name__=="__main__":
+    main()
